038c14562eeb8d7eab75f8f41fc778a7d5f7c09d
sjoblomd <sjoblomd@arcada.fi>
2025-04-15 15:57:12 +0300

    Code challenge - Settings activity and shared preferences

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,35 +1,104 @@
 package fi.arcada.codechallenge;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.Locale;
+import android.content.SharedPreferences;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+// Denna MainActivity håller koll på hur många gånger man öppnat appen
+// Den säger även två olika meddelanden - En vid start och en efter man varit i settings
 public class MainActivity extends AppCompatActivity {
+    // En boolean för att hålla koll på om vi just starta appen
+    private boolean isInitialLaunch = true;
+    private TextView appCountTextView;
 
-    private ArrayList<Double> numbers;
+    private String textInput;
+    private TextView welcomeTextView;
+    private FloatingActionButton settingsButton;
 
+    // Vi gör en instans av SharedPreferences
+    private SharedPreferences updateText;
+    private SharedPreferences sharedPreferences;
+    // Man kan ha många olika shared preferences
+    // En sharedPrefence är som cookies i webdev
+    // Man spara sen nyckel-värd par (lite som HashMaps från tidigare under lektionen)
+    // Vi gör en sharedPreferences för våra "counters" (räknare)
+    // Vi gör sen också vår "räknare"
+
+    private static final String TEXT_INPUT = "TextInput";
+    private static final String PREFS_NAME = "AppCounterPrefs";
+    private static final String APP_COUNTER_KEY = "appCounter";
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView resultText = findViewById(R.id.resultText);
-        Button calculateBtn = findViewById(R.id.calculateBtn);
+        appCountTextView = findViewById(R.id.appCount);
+        welcomeTextView = findViewById(R.id.welcomeView);
 
-        numbers = new ArrayList<>();
-        numbers.add(4.0);
-        numbers.add(7.0);
-        numbers.add(10.0);
+        // Vår settings button - Med snygg icon
+        settingsButton = findViewById(R.id.myFloatingActionButton);
 
-        calculateBtn.setOnClickListener(view -> {
-            double mean = Statistics.calcMean(numbers);
-            String formatted = String.format(Locale.US, "Mean: %.2f", mean);
-            resultText.setText(formatted);
-        });
+        // Här öppnar vi den specifika sharedPreferences filen vi definerat (PREFS_NAME dvs. AppCounterPrefs)
+        // Vi läser den sedan i privat läge (Default) så vår klass har tillgång till den
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        updateText = getSharedPreferences(TEXT_INPUT, MODE_PRIVATE);
+
+        // En enkel intro text
+        welcomeTextView.setText("Hej på dig!");
+
+        // Hämta värdet som just nu finns i vår nyckel APP_COUNTER_KEY
+        int appCounter = sharedPreferences.getInt(APP_COUNTER_KEY, 0);
+
+        textInput = updateText.getString(TEXT_INPUT, "");
+
+        // Öka värdet (vi har ju just öppnat appen och denna onCreate() körs)
+        appCounter++;
+
+        // Spara det nya värdet med edit & apply
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(APP_COUNTER_KEY, appCounter);
+        editor.apply();
+
+        // Vi visar vår fina counter i vår textView
+        appCountTextView.setText(String.valueOf(appCounter));
+
+
+
+        // Vår clickListener för settings
+        settingsButton.setOnClickListener(v -> openSettings());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // onResume kallas varje gång denna Activity kommer i fokus
+        // Vi kan använda vår boolean för att se till att saker funkar när vi kommer från settings
+        if (!isInitialLaunch) {
+
+            welcomeTextView.setText(textInput);
+        } else {
+            isInitialLaunch = false;
+        }
+
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
+
+
+
+
+
+
--- app/src/main/java/fi/arcada/codechallenge/Statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -2,15 +2,22 @@ package fi.arcada.codechallenge;
 
 import java.util.ArrayList;
 
-public class Statistics {
-    public static double calcMean(ArrayList<Double> data) {
-        if (data == null || data.isEmpty()) return 0;
+public class statistics {
 
+    public static double calcMean(ArrayList<Double> values){
         double sum = 0;
-        for (double num : data) {
+        for(double num : values){
             sum += num;
-        }
 
-        return sum / data.size();
+        }
+        return sum/ values.size();
+
     }
+
+
+
+
+
 }
+
+
